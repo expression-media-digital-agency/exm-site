a6e10f20c8d5d10defe7aea77b750efabf6c0e47
ExpressionMedia <51117652+ExpressionMedia@users.noreply.github.com>
2021-08-09 15:52:17 +0100

    new content

--- pages/about.js
+++ pages/about.js
@@ -7,10 +7,12 @@ import NeedSolutionOne from "../components/NeedSolutionOne/NeedSolutionOne";
 
 const About = () => <Layout>
 <Head>
-    <title>ExMedia Digital | Grow your Startup</title>
+    <title>ExMedia Creatives | MarTech at its Best</title>
+    <meta name="facebook-domain-verification" content="1hwsc7wgie2zrcgqpb0ycvu2g4uxcb" />
     <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport"/>
-        <meta name="description" content="Digital Marketing for Startups" />
-            <meta name="keywords" content="social media marketing, startup marketing, grow your startup, startup revenue,social media, marketing, digital, SEO, increase profit, top marketing firm, best agency, revenue " />
+        <meta name="google-site-verification" content="kmbrx57bxpG7mnv9qSUcjLAbULlGQwDRthw8Zid_gyY" />
+            <meta name="description" content="Digital Marketing for Startups" />
+            <meta name="keywords" content="social media marketing, marketing technology, best of marketing, startup marketing, grow your startup, startup revenue,social media, marketing, digital, SEO, increase profit, top marketing firm, best agency, revenue " />
             <meta name="author" content="Expression Media" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"></link>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossOrigin="anonymous"></link>
@@ -51,8 +53,8 @@ const About = () => <Layout>
             <div className="col-sm-1"></div>
             <div className="col-sm-5">
                 <p className="paragraph-one">
-                    Our vision is to help build profitable startups, though data-driven Marketing.<br/><br/>
-                    Our focus is to make startups profitable, by helping startups generate and multiply sales online. <br/>
+                    Our vision is to make Startups Swiftly and Increasingly Profitable through Marketing Technology.<br/><br/>
+                
                     Our commitment is to help startups overcome the challenge of getting adequate sales for their products and services.
                 </p>
             </div>
@@ -64,8 +66,8 @@ const About = () => <Layout>
             <div className="col-sm-1"></div>
             <div className="col-sm-5">
                 <p className="paragraph-one">
-                Our goal is simple: to make your startup goals become a reality.<br/><br/>      
-                We want to partner with you to grow the vision for your startup.<br/>
+                Our goal is simple: to make your Startup Financial Goals become a reality.<br/><br/>      
+                We want to partner with you to Grow, by building a Dedicated and Exclusive Marketing Team for your Startup.<br/>
                 We are committed to going all the way with you to help you meet your revenue targets in record time. While you are engaged with designing and building your products and services, we are working with you to creatively and efectively market them. .
                 </p>
             </div>
@@ -86,7 +88,7 @@ const About = () => <Layout>
             <div className="col-sm-1"></div>
             <div className="col-sm-5">
                 <p className="paragraph-one">
-                We are a team of marketing experts that will help your Startup to generate & multiply sales for your products and services. Our Team will help you get better results with your current marketing budget.<br/><br/>
+                We are a team of Marketing Technology experts that will help your Startup to generate & multiply sales for your products and services. Our Team will help you get better results with your current marketing budget.<br/><br/>
 
                 Our team of marketing experts is made up of Strategists, Display Ads professionals, Social Media Auditors, Social Media Marketers, Visual experts, 
                     Ads Campaign Designers, Content Writers, and much more.
